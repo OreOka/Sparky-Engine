a003d6ab9084204b231a36c6aabb36f79acdc7b1
Oreva Okaruefe <orevaokaruefe@gmail.com>
2022-04-12 15:44:39 -0500

    Shaders have been added now

--- Sparky~core/main.cpp
+++ Sparky~core/main.cpp
@@ -1,24 +1,25 @@
 #include "src/graphics/window.h"
 #include "src/graphics/shader.h"
 #include "src/maths/maths.h"
-#include "src/utils/fileutils.h"
-int main() {
+
+int main()
+{
 	using namespace sparky;
 	using namespace graphics;
 	using namespace maths;
 
-	
-	Window window("Sparky!", 800, 800);
-	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+	Window window("Sparky!", 960, 540);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-
-
-	GLfloat vertices[] = {
-		-0.5f, -0.5f,  0.0f,
-		-0.5f,  0.5f,  0.0f,
-		 0.5f,  0.5f,  0.0f,
-		 0.5f, -0.5f,  0.0f,
-		-0.5f, -0.5f,  0.0f
+	GLfloat vertices[] = 
+	{
+		// first triangle
+		0, 0, 0,
+		8, 0, 0,
+		0, 3, 0,
+		0, 3, 0,
+		8, 3, 0,
+		8, 0, 0
 	};
 
 	GLuint vbo;
@@ -28,20 +29,25 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
+	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 	shader.enable();
-	
-	while (!window.closed())	
+	shader.setUniformMat4("pr_matrix", ortho);
+	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+
+	shader.setUniform2f("light_pos", vec2(8.0f, 1.5f));
+	shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
+
+	while (!window.closed())
 	{
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(-0.5f, 0.5f);
-		glVertex2f(0.5f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-
 		window.clear();
+		double x, y;
+		window.getMousePosition(x, y);
+		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-
 		window.update();
 	}
+
 	return 0;
 }
--- Sparky~core/src/shaders/basic.frag
+++ Sparky~core/src/shaders/basic.frag
@@ -2,7 +2,13 @@
 
 layout (location = 0 ) out vec4 color;
 
+uniform vec4 colour;
+uniform vec2 light_pos;	
+
+in vec4 pos;
+
 void main()
 {
-	gl_FragColor = vec4(1.0, 0.5, 1.0, 1.0);
+	float intensity = 1.0/length(pos.xy - light_pos);
+	color = colour *intensity;
 }
