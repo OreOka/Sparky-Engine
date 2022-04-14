21b830dc3eaf0a2c8666f1c8df89982db71b3a06
Oreva Okaruefe <orevaokaruefe@gmail.com>
2022-04-14 14:29:40 -0500

    Layers

--- Sparky~core/src/graphics/window.cpp
+++ Sparky~core/src/graphics/window.cpp
@@ -62,6 +62,7 @@ namespace sparky  { namespace graphics {
 		glfwSetKeyCallback(m_Window, key_callback);
 		glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
 		glfwSetCursorPosCallback(m_Window, cursor_position_callback);
+		glfwSwapInterval(0);
 
 		if (glewInit() !=GLEW_OK) {
 			std::cout << "Could not initialize GLEW!" << std::endl;
