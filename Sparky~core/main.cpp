4c5deb6c2b71af8dda127fb90ff6f4ecb1fc2262
Oreva Okaruefe <orevaokaruefe@gmail.com>
2022-04-20 10:17:54 -0500

    OpenGL Texures

--- Sparky~core/main.cpp
+++ Sparky~core/main.cpp
@@ -9,21 +9,22 @@
 #include "src/graphics/renderer2d.h"
 #include "src/graphics/simple2drenderer.h"
 #include "src/graphics/batchrenderer2d.h"
-#include "src/graphics/batchrenderer2D.h"
 
 #include "src/graphics/static_sprite.h"
 #include "src/graphics/sprite.h"
 #include "src/utils/timer.h"
-#include <time.h>
 
 #include "src/graphics/layers/tilelayer.h"
+
 #include "src/graphics/layers/group.h"
-#include <FreeImage.h>
+#include "src/graphics/texture.h"
+
+#include <time.h>
 
 #define BATCH_RENDERER 1
 #define TEST_50K_SPRITES 0
 
-#if 0
+#if 1
 int main()
 {
 	using namespace sparky;
@@ -55,54 +56,69 @@ int main()
 		}
 	}
 #else
-	Group* group = new Group(mat4::translation(maths::vec3(-15.0f, 10.0f, 0.0f)));
-	group->add(new Sprite(0, 0, 6, 3, maths::vec4(1, 1, 1, 1)));
 
+	Group* group = new Group(mat4::translation(maths::vec3(-15.0f, 5.0f, 0.0f)));
+	group->add(new Sprite(0, 0, 6, 3, maths::vec4(1, 1, 1, 1)));
+	
 	Group* button = new Group(mat4::translation(vec3(0.5f, 0.5f, 0.0f)));
 	button->add(new Sprite(0, 0, 5.0f, 2.0f, maths::vec4(1, 0, 1, 1)));
 	button->add(new Sprite(0.5f, 0.5f, 3.0f, 1.0f, maths::vec4(0.2f, 0.3f, 0.8f, 1)));
 	group->add(button);
-	
+
 	layer.add(group);
 
 #endif
 
-	/*TileLayer layer2(&shader2);
-	layer2.add(new Sprite(-2, -2, 4, 4, maths::vec4(1, 0, 1, 1)));*/
+	TileLayer layer2(&shader2);
+	layer2.add(new Sprite(-2, -2, 4, 4, maths::vec4(1, 0, 1, 1)));
+	glActiveTexture(GL_TEXTURE0);
+	Texture texture("test.png");
+	texture.bind();
 
+	shader.enable();
+	shader.setUniform1i("tex", 0);
+	shader.setUniformMat4("pr_matrix", maths::mat4::orthographic(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
 	Timer time;
 	float timer = 0;
 	unsigned int frames = 0;
-
 	while (!window.closed())
 	{
 		window.clear();
-		double x, y;
+/*		double x, y;
 		window.getMousePosition(x, y);
-		shader.enable();
 		shader.setUniform2f("light_pos", vec2((float)(x * 32.0f / 960.0f - 16.0f), (float)(9.0f - y * 18.0f / 540.0f)));
 		//shader.setUniform2f("light_pos", vec2((float)(x * 32.0f / 960.0f - 16.0f), (float)(9.0f - y * 18.0f / 540.0f)));
 		//shader.setUniform2f("light_pos", vec2(-8, -3));
 		shader2.enable();
+		layer.render();*/
+	//	layer2.render();
 
-		layer.render();
-		//	layer2.render();
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(0, 0);
+		glTexCoord2f(0, 1);
+		glVertex2f(0, 4);
+		glTexCoord2f(1, 1);
+		glVertex2f(4, 4);
+		glTexCoord2f(1, 0);
+		glVertex2f(4, 0);
+		glEnd();
 
 		window.update();
 		frames++;
-
-		if (time.elapsed() - timer > 1.0f) {
+		if (time.elapsed() - timer > 1.0f)
+		{
 			timer += 1.0f;
-			printf("%d fps \n", frames);
+			printf("%d fps\n", frames);
 			frames = 0;
 		}
 	}
-
+	
 	return 0;
 }
-
 #endif
 
+#if 0
 int main()
 {
 	const char* filename = "test.png";
@@ -110,7 +126,7 @@ int main()
 	//image format
 	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
 	//pointer to the image, once loaded
-	FIBITMAP* dib(0);
+	FIBITMAP *dib(0);
 	//pointer to the image data
 	BYTE* bits(0);
 	//image width and height
@@ -139,7 +155,7 @@ int main()
 	unsigned int bitsPerPixel = FreeImage_GetBPP(dib);
 	unsigned int pitch = FreeImage_GetPitch(dib);
 	//get the image width and height
-
+	
 	width = FreeImage_GetWidth(dib);
 	height = FreeImage_GetHeight(dib);
 	//if this somehow one of these failed (they shouldn't), return failure
@@ -148,16 +164,17 @@ int main()
 
 	for (int y = height; y > 0; y--)
 	{
-		BYTE* pixel = (BYTE*)bits;
+		BYTE *pixel = (BYTE*)bits;
 		for (int x = 0; x < width; x++)
 		{
-			std::cout << +pixel[FI_RGBA_RED] << " " << +pixel[FI_RGBA_GREEN] << +pixel[FI_RGBA_BLUE] << std::endl;
+			std::cout << +pixel[FI_RGBA_RED] << " " << +pixel[FI_RGBA_GREEN] << " " << +pixel[FI_RGBA_BLUE] << std::endl;
 			pixel += 3;
 		}
-
-		//next line
-		bits += pitch;
-	}
+		// next line
+		bits += pitch; 
+	} 
+	FreeImage_Unload(dib);
 
 	return 0;
 }
+#endif
